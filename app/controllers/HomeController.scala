d525c7960b5935e0d3b2969f55b2785ce37a07e7
BernhardGundel <bernhard.gundel@htwg-konstanz.de>
2020-10-15 18:20:03 +0200

    Changed dependencies in build.sbt. Changed HomeController.scala.

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,22 +3,23 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import de.htwg.se.shutthebox.ShutTheBox
+import de.htwg.se.shutthebox.controller.controllerComponent.ControllerInterface
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
 @Singleton
 class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+  val gameController: ControllerInterface = ShutTheBox.controller
+  private val shutTheBoxAsText: String = gameController.toString()
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
+  def about: Action[AnyContent] = Action {
+    Ok(views.html.index())
+  }
+
+  def shutthebox: Action[AnyContent] = Action {
+    Ok(shutTheBoxAsText)
+  }
+
+  def index(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
 }
