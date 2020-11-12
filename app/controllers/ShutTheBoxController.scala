fe44f97ef419e31b023214601b083ca5a66ac264
BernhardGundel <bernhard.gundel@htwg-konstanz.de>
2020-11-12 13:31:21 +0100

    Added some bootstrap elements. Implemented undo and redo. Implemented ingame error messages.

--- app/controllers/ShutTheBoxController.scala
+++ app/controllers/ShutTheBoxController.scala
@@ -8,6 +8,9 @@ import de.htwg.se.shutthebox.controller.controllerComponent.ControllerInterface
 @Singleton
 class ShutTheBoxController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
   val gameController: ControllerInterface = ShutTheBox.controller
+  var errDice: Boolean = false
+  var errShut: Boolean = false
+  var errShutRoll: Boolean = false
 
   def shutthebox: Action[AnyContent] = Action {
     Ok(views.html.mainmenu())
@@ -19,27 +22,62 @@ class ShutTheBoxController @Inject()(cc: ControllerComponents) extends AbstractC
 
   def ingame(): Action[AnyContent] = Action {
     gameController.startGame(1, ai = false)
-    Ok(views.html.ingame(gameController))
+    Ok(views.html.ingame(gameController, errDice, errShut, errShutRoll))
   }
 
   def doShut(i:Int): Action[AnyContent] = Action {
-    gameController.doShut(i)
-    Ok(views.html.ingame(gameController))
+    val result = gameController.doShut(i)
+    if (result == "This shut is not allowed") {
+      errDice = false
+      errShut = true
+      errShutRoll = false
+    } else {
+      errDice = false
+      errShut = false
+      errShutRoll = false
+    }
+    if (result == "Please roll the dice first!") {
+      errDice = false
+      errShutRoll = true
+    } else {
+      errDice = false
+      errShutRoll = false
+    }
+    Ok(views.html.ingame(gameController, errDice, errShut, errShutRoll))
   }
 
   def rollDice: Action[AnyContent] = Action {
-    gameController.rollDice
-    Ok(views.html.ingame(gameController))
+    val result: String = gameController.rollDice
+    if (result == "Dice roll not allowed!") {
+      errDice = true
+      errShut = false
+      errShutRoll = false
+    } else {
+      errDice = false
+      errShut = false
+      errShutRoll = false
+    }
+    Ok(views.html.ingame(gameController, errDice, errShut, errShutRoll))
   }
 
   def nextPlayer: Action[AnyContent] = Action {
     gameController.setCurrentPlayer()
     if (gameController.currentPlayerIndex < 2) {
-      Ok(views.html.ingame(gameController))
+      Ok(views.html.ingame(gameController, errDice, errShut, errShutRoll))
     } else {
       val players = gameController.getPlayers
       Ok(views.html.scoreboard(players(0).score, players(1).score))
     }
+  }
+
+  def undo: Action[AnyContent] = Action {
+    gameController.undoShut()
+    Ok(views.html.ingame(gameController, errDice, errShut, errShutRoll))
+  }
+
+  def redo: Action[AnyContent] = Action {
+    gameController.redoShut()
+    Ok(views.html.ingame(gameController, errDice, errShut, errShutRoll))
   }
 
 }
