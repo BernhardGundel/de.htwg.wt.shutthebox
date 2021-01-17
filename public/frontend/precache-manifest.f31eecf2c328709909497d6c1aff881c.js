36c92f9496c2c6cde78a9695bcb58a0230bebcae
BernhardGundel <bernhard.gundel@htwg-konstanz.de>
2021-01-17 22:27:49 +0100

    Updated frontend again.

--- public/frontend/precache-manifest.f31eecf2c328709909497d6c1aff881c.js
+++ public/frontend/precache-manifest.f31eecf2c328709909497d6c1aff881c.js
@@ -1,66 +1,66 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7cb4917095450d596bfa",
-    "url": "/frontend/css/app.c06a9331.css"
+    "revision": "9235d431bdb6005746a5",
+    "url": "/css/app.c06a9331.css"
   },
   {
     "revision": "5a143165afe3d3dab09f",
-    "url": "/frontend/css/chunk-vendors.c5b0137c.css"
+    "url": "/css/chunk-vendors.c5b0137c.css"
   },
   {
     "revision": "caf4adf1dc917c926e5cec4de86f4bf2",
-    "url": "/frontend/img/arrow-left.caf4adf1.svg"
+    "url": "/img/arrow-left.caf4adf1.svg"
   },
   {
     "revision": "f5921019722c0cd799c862993e0a1f6f",
-    "url": "/frontend/img/crown.f5921019.svg"
+    "url": "/img/crown.f5921019.svg"
   },
   {
     "revision": "e0c09975e35a7699c5b15d5e98ae4f70",
-    "url": "/frontend/img/mute.e0c09975.svg"
+    "url": "/img/mute.e0c09975.svg"
   },
   {
     "revision": "ed9300857243cb6dc1322699fb22cc93",
-    "url": "/frontend/img/volume.ed930085.svg"
+    "url": "/img/volume.ed930085.svg"
   },
   {
-    "revision": "cfa98ab9e0d9a8e70b5a60b2258bd66c",
-    "url": "/frontend/index.html"
+    "revision": "285a3e551666d19a4d6aff13b4749ebd",
+    "url": "/index.html"
   },
   {
-    "revision": "7cb4917095450d596bfa",
-    "url": "/frontend/js/app.3f737ccb.js"
+    "revision": "9235d431bdb6005746a5",
+    "url": "/js/app.cd4b6ba9.js"
   },
   {
     "revision": "5a143165afe3d3dab09f",
-    "url": "/frontend/js/chunk-vendors.a572b51a.js"
+    "url": "/js/chunk-vendors.a572b51a.js"
   },
   {
     "revision": "abcd7624b67ab7f0117077e2fe75ccf1",
-    "url": "/frontend/manifest.json"
+    "url": "/manifest.json"
   },
   {
     "revision": "fc4c6bc0dbf2d3d89230b4fbe100f75c",
-    "url": "/frontend/media/aaw.fc4c6bc0.mp3"
+    "url": "/media/aaw.fc4c6bc0.mp3"
   },
   {
     "revision": "537cc32515cf3cc6a9027c68a0825a31",
-    "url": "/frontend/media/btn.537cc325.mp3"
+    "url": "/media/btn.537cc325.mp3"
   },
   {
     "revision": "1ed8a77a914280b7c9156341ba60fd91",
-    "url": "/frontend/media/cheer.1ed8a77a.mp3"
+    "url": "/media/cheer.1ed8a77a.mp3"
   },
   {
     "revision": "c902feb245d694876a33f9a6a0c2a37a",
-    "url": "/frontend/media/rollDice.c902feb2.mp3"
+    "url": "/media/rollDice.c902feb2.mp3"
   },
   {
     "revision": "e6080dabbc7b57a75bfa8967443a8053",
-    "url": "/frontend/media/shut.e6080dab.wav"
+    "url": "/media/shut.e6080dab.wav"
   },
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
-    "url": "/frontend/robots.txt"
+    "url": "/robots.txt"
   }
 ]);
--- public/frontend/service-worker.js
+++ public/frontend/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/frontend/precache-manifest.67fdc9c9ba552a93a8fada1194d20c1b.js"
+  "/precache-manifest.f31eecf2c328709909497d6c1aff881c.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "shutthebox"});
